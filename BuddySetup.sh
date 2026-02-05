e1d7f6cfd0c6923ad204d701ad407f78b4625283
crashputer <crashputer@gmail.com>
2026-02-05 13:00:29 -0500

    Update BuddySetup.sh

--- BuddySetup.sh
+++ BuddySetup.sh
@@ -1,16 +1,39 @@
 #!/bin/bash
 
+# SUDO check
+if [[ $UID != 0 ]]; then
+    echo "This script must be run with sudo:"
+    echo "sudo $0 $*"
+    exit 1
+fi
+
+# Start with a fully updated system
+apt update
+apt upgrade -y
+
+# Check if reboot is necessary before continuing
+if [ -f /var/run/reboot-required ]; then
+    echo "***--------------------------------------------------------------***"
+    echo "Reboot required. Please reboot the computer then restart the script."
+    echo "***--------------------------------------------------------------***"
+    exit 1
+else
+    echo "No reboot needed. Continuing..."
+    exit 0
+fi
+
+# Install all necessary packages
+apt install samba rclone -y
+
 # This will become a proper script. I'm just getting things written down.
 
-sudo apt update && sudo apt upgrade -y
-sudo apt install samba rclone -y
-sudo chown -R buddy:buddy /usr/local/local
-sudo chmod -R 755 /usr/local/local
-sudo chown -R buddy:buddy /usr/local/remote
-sudo chmod -R 755 /usr/local/remote
+chown -R buddy:buddy /usr/local/local
+chmod -R 755 /usr/local/local
+chown -R buddy:buddy /usr/local/remote
+chmod -R 755 /usr/local/remote
 echo -e "[buddy]\\ncomment = Local Share\\npath = /usr/local/local\\nread only = no\\nbrowsable = yes\\nwritable = yes\\nguest ok = no\\nvalid users = buddy\\n\\n[remote]\\ncomment = Local Share\\npath = /usr/local/remote\\nread only = no\\nbrowsable = no\\nwritable = yes\\nguest ok = no\\nvalid users = buddy" | sudo tee -a /etc/samba/smb.conf
-sudo systemctl restart samba
+systemctl restart samba
 echo "..."
 echo "Enter a new SMB Password"
 echo "..."
-sudo smbpasswd -a buddy
+smbpasswd -a buddy
